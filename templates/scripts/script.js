f39022d1891ff50445e4d44d4d5a8bc14bdb13fd
Collin <collin@kernandlead.com>
2012-10-23 15:27:53 -0400

    Working on the utility panel and the mini cart

--- templates/scripts/script.js
+++ templates/scripts/script.js
@@ -7,9 +7,8 @@ $(document).ready(function() {
 	function offscreen () {
 		var $ostoggle = $('.os-toggle');
 	    var $ospanel = $('.os-panel');
-	    
 	    $ostoggle.click(function(e) {
-	        var ostarget = $(this).attr('href');
+	        var ostarget = $(this).data('target');
 	        $('body').toggleClass('active-os-panel');
             $(ostarget).toggleClass('is-showing');
             e.preventDefault();
