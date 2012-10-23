3a9f3019fab04e20b13917bc6e569693e04ce662
Collin <collin@kernandlead.com>
2012-10-23 10:31:33 -0400

    overall theme tightening. testing some featurette content. adding a quick test of the offscreen utility panel

--- templates/scripts/script.js
+++ templates/scripts/script.js
@@ -2,4 +2,21 @@ $(document).ready(function() {
    $('.popover-trigger').popover({delay: { show: 500, hide: 100 }});
 
    $(document).foundationCustomForms();
+
+
+	function offscreen () {
+		var $ostoggle = $('.os-toggle');
+	    var $ospanel = $('.os-panel');
+	    
+	    $ostoggle.click(function(e) {
+	        var ostarget = $(this).attr('href');
+	        $('body').toggleClass('active-os-panel');
+            $(ostarget).toggleClass('is-showing');
+            e.preventDefault();
+	    });
+	}
+
+
+	offscreen();
+
 });
